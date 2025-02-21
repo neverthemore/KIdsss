6230dc141cc046e2eda72c07bf4ede8d31c93bb6
Xlebusheck1 <coco.qwe.coco@gmail.com>
2025-02-21 03:18:36 +0300

    added animations of JUMP, SIT, RUN

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -12,10 +12,18 @@ public class Animations : MonoBehaviour
     {
         float inputLeft = Input.GetAxis("Horizontal");
         float inputFwd = Input.GetAxis("Vertical");
+
+        bool isSit = Input.GetKey(KeyCode.LeftControl);
+        float sit = isSit ? 1f : 0f;
+
+        float isJump = Input.GetAxis("Jump");
+        
         if (inputLeft != 0f || inputFwd != 0f)
         {
             animator.SetFloat("x", inputLeft);
             animator.SetFloat("y", inputFwd);
+            animator.SetFloat("sit", sit);
+            animator.SetFloat("jump", isJump);
         }
     }
 }
