4e34c3a3ec33dcdcc87a4e3345d1a2397e8d7719
NoPosh <murin8@mail.ru>
2025-02-24 18:45:29 +0300

    Camera
    -Добавлено управление мышкой
    -Поворот + камера

---
+++ Assets/Scripts/Character/CharacterInfo.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public enum CharacterState { Walk, Run, Chouch}  //Возможно + прыжок и идл
+
+    private CharacterState _currentState;
+    
+}
